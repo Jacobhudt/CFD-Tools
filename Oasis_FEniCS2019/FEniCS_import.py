caf341cd765a6b02d5a368a6fd56be675dc398cf
Jacobhudt <jacob.hudtwalcker@gmail.com>
2022-10-20 10:15:51 +0200

    various updates

--- Oasis_FEniCS2019/FEniCS_import.py
+++ Oasis_FEniCS2019/FEniCS_import.py
@@ -1,27 +1,31 @@
 from fenics import *
 import sys
 
-
 # Read volumetric mesh
 mesh = Mesh()
-XDMFFile("YOURMESH_domain.xdmf").read(mesh)
 
 # Read tags for volume elements
-cell_markers = MeshFunction("size_t", mesh, mesh.topology().dim())
-XDMFFile("YOURMESH_domain.xdmf").read(cell_markers)
-with XDMFFile("cell_markers.xdmf") as f:
-    f.write(cell_markers)
+mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim())
+with XDMFFile(MPI.comm_world, "YOUR_MESH_DOMAIN.xdmf") as infile:
+   infile.read(mesh)
+   infile.read(mvc, "name_to_read")
 
 # Read tags for surface elements (can also be used for applying DirichletBC)
-boundaries = MeshValueCollection("size_t", mesh, mesh.topology().dim() - 1)
-XDMFFile("YOURMESH_boundary.xdmf").read(boundaries)
-boundaries = MeshFunction("size_t", mesh, boundaries)
 
-##Example boundary conditions
+mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim()-1)
+with XDMFFile(MPI.comm_world, "YOUR_MESH_BOUNDARY.xdmf") as infile:
+    infile.read(mvc, "name_to_read")
+
+mf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+dx = Measure("dx")(subdomain_data=mf)
+
+
+
+##Example boundary conditions, inspect boundary tags visiually for example in paraview
 """
-inlet = DirichletBC(V, Constant(1), boundaries, 6) #6 is the key for the inlet-boundary
-outlet = DirichletBC(V, Constant(-1), boundaries, 7)
-wall = DirichletBC(V, Constant(0), boundaries, 8)
+inlet = DirichletBC(V, Constant(1), boundaries, 1) #6 is the key for the inlet-boundary
+outlet = DirichletBC(V, Constant(-1), boundaries, 2)
+wall = DirichletBC(V, Constant(0), boundaries, 3)
 
 """
 
--- Oasis_FEniCS2019/convertMEDToXDMF.py
+++ Oasis_FEniCS2019/convertMEDToXDMF.py
@@ -1,29 +1,49 @@
 import meshio
 import sys
-
+import numpy as np
 """ Script for converting Salome .med files to .xdmf files readable by FEniCS.
     Mesh gets separated into two files as FEniCS cannot work with mixed cells. (triangles and tetrahedrons)
     The predefined boundaries in the mesh gets assigned an index starting with 6,7,...
     To ensure correct boundary definitions, *_boundary.xdmf should be inspected visually in Paraview.
 """
 
-
 ## Run with: python convertMEDtoXDMF.py mesh_name.med
 
 mesh = meshio.read(sys.argv[1])
 
-mesh.cell_data_dict["cell_tags"]["tetra"] *= -1
+print(len(mesh.cell_data["cell_tags"][2]))
+
+temp_b = np.copy(mesh.cell_data["cell_tags"][2])
+
+temp_d = np.copy(mesh.cell_data["cell_tags"][1])
+
+## Convert the negative boundary tags (which usually starts with 6), to 1,2,...
+
+for i in range(len(temp_b)):
+    if temp_b[i] == -6:
+        temp_b[i] = 3
+    if temp_b[i] == -7:
+        temp_b[i] = 2
+    if temp_b[i] == -8:
+        temp_b[i] = 1
+
+## Convert cells tagged by 0 to 1. Only valid for meshes with one region.
+
+for i in range(len(temp_d)):
+    temp_d[i] = 1
 
 
 meshio.write_points_cells(sys.argv[1].replace('.med', '') + "_boundary.xdmf",
     mesh.points,
-    [mesh.cells[1]],
-    cell_data={"f": [mesh.cell_data["cell_tags"][1][:]*(-1)]}, 
+    [mesh.cells[2]],
+    cell_data={"name_to_read": [temp_b]}, # "name_to_read" is an arbitrary name for the tags used in most FEniCS tutorials
 )
 
 
 meshio.write_points_cells(sys.argv[1].replace('.med', '') + "_domain.xdmf",
     mesh.points,
-    [mesh.cells[0]],
-    cell_data={"f": [mesh.cell_data["cell_tags"][0]]},
+    [mesh.cells[1]],
+    cell_data={"name_to_read": [temp_d]}, # "name_to_read" is an arbitrary name for the tags used in most FEniCS tutorials
 )
+
+
